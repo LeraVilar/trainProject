f7496df3424c1ff4a5c2b7466f72057d6d71b84c
Veldriant <veldriant3021@gmail.com>
2023-09-02 14:03:07 +0300

    fix

--- js.js
+++ js.js
@@ -3,3 +3,64 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 4,
     gap:40,
   });
+
+
+let parent = document.querySelector('.container');
+let menuItem = parent.querySelectorAll('.menu__item');
+
+parent.addEventListener('click', (event) => {
+  // Отлавливаем элемент в родители на который мы нажали
+  let target = event.target;
+  
+  // Проверяем тот ли это элемент который нам нужен
+  if(target.classList.contains('menu__item')) {
+    for(let i = 0; i < menuItem.length; i++) {
+      // Убираем у других
+      menuItem[i].classList.remove('active');
+    }
+    // Добавляем тому на который нажали
+    target.classList.add('active');
+  }
+  
+});
+
+parent.addEventListener('click', (event) => {
+  // Отлавливаем элемент в родители на который мы нажали
+  let target = event.target;
+  if(document.getElementsByClassName('menu__item1 active').length > 0) {
+    document.getElementsByClassName('menu__content1')[0].classList.add('active');
+  }
+  if(document.getElementsByClassName('menu__item1 active').length < 1) {
+    document.getElementsByClassName('menu__content1')[0].classList.remove('active');
+  }
+});
+parent.addEventListener('click', (event) => {
+  // Отлавливаем элемент в родители на который мы нажали
+  let target = event.target;
+  if(document.getElementsByClassName('menu__item2 active').length > 0) {
+    document.getElementsByClassName('menu__content2')[0].classList.add('active');
+  }
+  if(document.getElementsByClassName('menu__item2 active').length < 1) {
+    document.getElementsByClassName('menu__content2')[0].classList.remove('active');
+  }
+});
+parent.addEventListener('click', (event) => {
+  // Отлавливаем элемент в родители на который мы нажали
+  let target = event.target;
+  if(document.getElementsByClassName('menu__item3 active').length > 0) {
+    document.getElementsByClassName('menu__content3')[0].classList.add('active');
+  }
+  if(document.getElementsByClassName('menu__item3 active').length < 1) {
+    document.getElementsByClassName('menu__content3')[0].classList.remove('active');
+  }
+});
+parent.addEventListener('click', (event) => {
+  // Отлавливаем элемент в родители на который мы нажали
+  let target = event.target;
+  if(document.getElementsByClassName('menu__item4 active').length > 0) {
+    document.getElementsByClassName('menu__content4')[0].classList.add('active');
+  }
+  if(document.getElementsByClassName('menu__item4 active').length < 1) {
+    document.getElementsByClassName('menu__content4')[0].classList.remove('active');
+  }
+});
