5e21e0a1e8381bdee77c530546f0468e91a731af
Veldriant <veldriant3021@gmail.com>
2023-09-04 12:34:24 +0300

    fix

--- js.js
+++ js.js
@@ -11,6 +11,28 @@ const swiper2 = new Swiper('.swiper2', {
       nextEl: ".custom-next",
     },
     loop: true,
+    breakpoints: {
+      00: {
+        slidesPerView: 1.9,
+        slidesPerGroup: 1,
+       },
+      720: {
+          slidesPerView: 2.2,
+          slidesPerGroup: 2,
+      },
+      990: {
+          slidesPerView: 2.5,
+          slidesPerGroup: 2,
+      },
+      1050: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+      },
+      1250: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+      },
+    }
   });
 
 // let parent = document.querySelector('.container');
