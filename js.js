2a868615abea171880b5683c886e6bc9ee410599
Veldriant <veldriant3021@gmail.com>
2023-09-04 13:00:31 +0300

    fix

--- js.js
+++ js.js
@@ -2,6 +2,28 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 4,
     spaceBetween: 30,
+    breakpoints: {
+      00: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+       },
+      720: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+      },
+      990: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+      },
+      1050: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+      },
+      1250: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+      },
+    }
   });
 const swiper2 = new Swiper('.swiper2', {
     slidesPerView: 4,
