c12c6a9a691e6e14a6b6f27ef12d4414342ffa86
Veldriant <veldriant3021@gmail.com>
2023-09-06 15:41:46 +0300

    fix

--- js.js
+++ js.js
@@ -11,13 +11,13 @@ const swiper = new Swiper('.swiper', {
           slidesPerView:1.5,
           slidesPerGroup: 1,
       },
-      990: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-      },
-      1050: {
+      500: {
           slidesPerView: 3,
           slidesPerGroup: 3,
+      },
+      990: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
       },
       1250: {
           slidesPerView: 4,
@@ -60,4 +60,43 @@ const swiper2 = new Swiper('.swiper2', {
     
   });
 
-  
+
+let tab = function () {
+  let tabNav = document.querySelectorAll('.accordeon-item'),
+      tabContent = document.querySelectorAll('.accordeon-title'),
+      closeBtn = document.querySelectorAll('.close'),
+      base = document.querySelector('.accordeon-base'),
+      tabName;
+      
+      tabNav.forEach(item => {
+          item.addEventListener('click', selectTabNav)
+          
+      });
+      closeBtn.forEach(btn => {
+        btn.addEventListener('click', function () {
+            console.log('click')
+            tabContent.forEach(closeItem => {
+              closeItem.classList.remove('active')
+              base.classList.add('active')
+            })
+        })
+    })
+
+      function selectTabNav() {
+        base.classList.remove('active');
+          tabNav.forEach(item => {
+              item.classList.remove('active');
+          });
+          this.classList.add('active');
+          tabName = this.getAttribute('data-tab-name');
+          selectTabContent(tabName);
+      }
+
+      function selectTabContent(tabName) {
+          tabContent.forEach(item => {
+              item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
+          })
+      }
+}
+
+tab();
